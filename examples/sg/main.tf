2ca9233b11c769d7217d23e85df89e94e6634ca9
leedonggyu <zkfmapf123@naver.com>
2025-11-02 12:37:28 +0900

    wip-50a518a5

--- examples/sg/main.tf
+++ examples/sg/main.tf
@@ -1,23 +1,23 @@
 resource "aws_security_group" "ex-1" {
-  name = "ex-1"
+  name = "ex-10"
   description = "Example Security Group"
   vpc_id = local.vpc_id  
 }
 
 resource "aws_security_group" "ex-2" {
-  name = "ex-2"
+  name = "ex-11"
   description = "Example Security Group"
   vpc_id = local.vpc_id  
 }
 
 resource "aws_security_group" "ex-3" {
-  name = "ex-3"
+  name = "ex-12"
   description = "Example Security Group"
   vpc_id = local.vpc_id  
 }
 
 resource "aws_security_group" "ex-4" {
-  name = "ex-3"
+  name = "ex-13"
   description = "Example Security Group"
   vpc_id = local.vpc_id  
 }
